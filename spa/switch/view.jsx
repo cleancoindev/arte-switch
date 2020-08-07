fec46f612cf4f4cf7caa64091e972b1333224dab
Marco Vasapollo <ceo@metaring.com>
2020-08-07 03:44:20 +0200

    revert

--- spa/switch/view.jsx
+++ spa/switch/view.jsx
@@ -38,14 +38,11 @@ var Switch = React.createClass({
     },
     render() {
         var _this = this;
-        if(!this.props.i) {
-            return (<Loader/>);
-        }
-        if (this.props[this.props.i].startBlock >= this.props[this.props.i].currentBlock) {
+        if (this.props.startBlock >= this.props.currentBlock) {
             return (<section className="switchBox">
                 <h3>Switch</h3>
                 <section className="switchTools">
-                    <h4>The Switch will be available at block <a target="_blank" href={window.getNetworkElement("etherscanURL") + "block/countdown/" + this.props[this.props.i].startBlock}>#{this.props[this.props.i].startBlock}</a></h4>
+                    <h4>The Switch will be available at block <a target="_blank" href={window.getNetworkElement("etherscanURL") + "block/countdown/" + this.props.startBlock}>#{this.props.startBlock}</a></h4>
                 </section>
             </section>);
         }
@@ -65,13 +62,13 @@ var Switch = React.createClass({
                     <img src={window.newToken.logo} />
                 </section>
                 <section className="switchActions">
-                    {!this.props[this.props.i].currentSlot && <Loader />}
-                    {this.props[this.props.i].currentSlot && window.walletAddress && <a href="javascript:;" className={"switchAction" + (!this.props[this.props.i].approved ? " active" : "")} onClick={this.approve}>Approve</a>}
-                    {this.props[this.props.i].currentSlot && window.walletAddress && <a href="javascript:;" className={"switchAction" + (this.props[this.props.i].approved ? " active" : "")} onClick={this.switch}>Switch</a>}
-                    {this.props[this.props.i].currentSlot && !window.walletAddress && <a href="javascript:;" onClick={() => window.ethereum.enable().then(() => window.getAddress()).then(() => _this.emit('ethereum/ping'))} className="switchAction active">Connect</a>}
+                    {!this.props.currentSlot && <Loader />}
+                    {this.props.currentSlot && window.walletAddress && <a href="javascript:;" className={"switchAction" + (!this.props.approved ? " active" : "")} onClick={this.approve}>Approve</a>}
+                    {this.props.currentSlot && window.walletAddress && <a href="javascript:;" className={"switchAction" + (this.props.approved ? " active" : "")} onClick={this.switch}>Switch</a>}
+                    {this.props.currentSlot && !window.walletAddress && <a href="javascript:;" onClick={() => window.ethereum.enable().then(() => window.getAddress()).then(() => _this.emit('ethereum/ping'))} className="switchAction active">Connect</a>}
                 </section>
-                {this.props[this.props.i].currentSlot && <p>Current Switch bonus is <b>X{window.numberToString(parseInt(this.props[this.props.i].currentSlot[1]) / parseInt(this.props[this.props.i].currentSlot[2]))}</b> until the block n. <a href={window.getNetworkElement("etherscanURL") + "block/" + this.props[this.props.i].currentSlot[0]} target="_blank">{this.props[this.props.i].currentSlot[0]}</a></p>}
-                {this.props[this.props.i].currentSlot && <p>Disclamer: Switching $BUIDL V1 to $buidl V2 is an irreversible action, do it at your own risk</p>}
+                {this.props.currentSlot && <p>Current Switch bonus is <b>X{window.numberToString(parseInt(this.props.currentSlot[1]) / parseInt(this.props.currentSlot[2]))}</b> until the block n. <a href={window.getNetworkElement("etherscanURL") + "block/" + this.props.currentSlot[0]} target="_blank">{this.props.currentSlot[0]}</a></p>}
+                {this.props.currentSlot && <p>Disclamer: Switching $BUIDL V1 to $buidl V2 is an irreversible action, do it at your own risk</p>}
             </section>
         </section>);
     }
